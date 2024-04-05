ddfc04ae4037160af3ea70c17d8eabeee3cf1f78
seaf26 <seafmohamedgamel@gmail.com>
2024-04-05 05:24:22 +0200

    i had finished all the media query , fixed the crud system and make functionable in the home page , fixed the sandwach it , need to adjust the search and the caet buttons

--- js/homepage.js
+++ js/homepage.js
@@ -1,46 +1,59 @@
-let navLinks = document.querySelectorAll('.nav ');
-let mmenu_iitem = document.querySelector("#hide");
-let mennnnu = document.getElementById("menu_homePage");
+let navLinks = document.querySelector('.nav ');
+let sandwich = document.getElementById('special');
 let icons = document.querySelectorAll(".icons ");
 let social_media= document.querySelectorAll(".share a");
 let img = document.querySelector('.welcomeHome .background img');
 let ListImages= ["images/home-bg.jpg",'images/pexels-maxime-francis-2246476.jpg',"images/pexels-irina-iriser-1366957.jpg","images/pexels-rompalli-harish-1698618.jpg","images/pexels-irina-iriser-1707213.jpg","images/pexels-carlos-oliva-3586966.jpg","images/pexels-roberto-nickson-2478248.jpg","images/pexels-piccinng-3075993.jpg","images/pexels-quang-nguyen-vinh-2162459.jpg","images/pexels-aleksandar-pasaric-2341830.jpg"];
 let cruuentIndex=0;
+let Datas=[]
+console.log(navLinks)
 
-console.log(mmenu_iitem); 
-console.log(mennnnu); 
-console.log(navLinks);
+if(window.localStorage.getItem("Products") !== null){
+    Datas=JSON.parse(window.localStorage.getItem("Products"))
+    loopData();
+}
 
 
-// console.log(mmenu_iitem);
-// console.log(social_media);
-console.log(mennnnu);
-// console.log(icons);
-// console.log(navLinks);
+
+function loopData() {
+    let theUsers = ``;
+    for (let i = 0; i < Datas.length; i++) {
+        theUsers += `
+                
+                <div class="product">
+                    <img src="${Datas[i].file}.jpg" alt="" />
+                    <h3>${Datas[i].ProductName}</h3>
+                    <p>Price: ${Datas[i].ProductPrice}</p>
+                    <button id="add" onclick="AddToCard(${i})">Add To Card</button>
+                </div>
+           
+                `;
+            }
+            document.getElementById("seaf").innerHTML = theUsers;
+}
+loopData();
+
+
 
 let screenWidth = window.innerWidth;
 window.addEventListener("resize", () => {
     screenWidth = window.innerWidth;
     if (screenWidth > 800) {
-        mmenu_iitem.style.display = "none";
-        mennnnu.style.display = "none";
-        navLinks.forEach(link => link.style.display = "flex");
-        icons.forEach(icon => icon.style.display = "block");
-        social_media.forEach(social => social.style.fontSize = "18px");//help
+        sandwich.style.display = "none";
+        // mennnnu.style.display = "none";
+        // navLinks.forEach(link => link.style.display = "flex");
+        // icons.forEach(icon => icon.style.display = "block");
+        // social_media.forEach(social => social.style.fontSize = "18px");
     } else {
-        navLinks.forEach(link => link.style.display = "none");
-        icons.forEach(icon => icon.style.display = "none");
-        mmenu_iitem.style.display = "flex";//help
+        sandwich.style.display = "flex";
+        // navLinks.forEach(link => link.style.display = "none");
+        // icons.forEach(icon => icon.style.display = "none");
+        navLinks.style.display = "none";
     }
 });
 
-mmenu_iitem.addEventListener("click", () => {
-    if (mennnnu.style.display === "none") {
-        mennnnu.style.display = "block";
-    } else {
-        mennnnu.style.display = "none";
-    }
-});
+
+
 
 
 
